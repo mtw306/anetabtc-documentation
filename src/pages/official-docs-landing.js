126691b7e462674c7aa000a88deed6a914e07846
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-29 21:45:15 -0500

    added banner back to docs

--- src/pages/official-docs-landing.js
+++ src/pages/official-docs-landing.js
@@ -6,17 +6,13 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import LandingSocials from '../components/LandingSocials'
 import Cards from '../pages/landing.js';
-import HomepageFeatures from '../components/HomepageFeatures';
 
-function HomepageHeader() {
+function DocumentationHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-        </div>
+        <h1 className="docs_header">Welcome to anetaBTC Documentation!</h1>
       </div>
     </header>
   );
@@ -29,6 +25,7 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
+      <DocumentationHeader />
         <Cards/>
         <h2 style={{display: 'flex', justifyContent: 'center'}}>Join our community!</h2>
         <LandingSocials/>
