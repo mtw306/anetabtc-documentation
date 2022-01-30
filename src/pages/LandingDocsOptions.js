089ecb585e10aba0f398894f7b813619675ea27e
Marguerite Blair <marguerite.blair@gmail.com>
2022-01-29 21:59:31 -0500

    official docs landing

--- src/pages/LandingDocsOptions.js
+++ src/pages/LandingDocsOptions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import styles from './docs-landing.css';
+import styles from './LandingDocsOptions.module.css';
 import Link from "@docusaurus/core/lib/client/exports/Link";
 import ExternalArrow from '../../static/img/external-arrow.svg'
 
