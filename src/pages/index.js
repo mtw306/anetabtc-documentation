5e7f35db4295d9137f0c12b0991941e23aa777fa
Marguerite Bliar <marguerite.blair@gmail.com>
2022-02-06 11:16:29 -0500

    cleanup and switching documentation landing to be on the homepage instead of a separate docs page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,32 @@ import LandingSocials from '../components/LandingSocials'
 import Cards from '../pages/landing.js';
 import HomepageFeatures from '../components/HomepageFeatures';
 
-function HomepageHeader() {
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           {/* <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/intro">
+//             Docusaurus Tutorial - 5min ⏱️
+//           </Link> */}
+//         </div>
+//         {/* <h2 style= {{marginBottom: 25}}></h2> */}
+//       </div>
+//     </header>
+//   );
+// }
+
+function DocumentationHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          {/* <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link> */}
-        </div>
-        {/* <h2 style= {{marginBottom: 25}}></h2> */}
+        <h1 className="docs_header">Welcome to anetaBTC Documentation!</h1>
       </div>
     </header>
   );
@@ -34,9 +45,11 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      {/* <HomepageHeader /> */}
       <main>
-        <HomepageFeatures />
+        <Cards/>
+        <h2 style={{display: 'flex', justifyContent: 'center'}}>Join our community!</h2>
+        <LandingSocials/>
       </main>
     </Layout>
   );
--- src/pages/official-docs-landing.js
+++ src/pages/official-docs-landing.js
@@ -6,16 +6,6 @@ import styles from './index.module.css';
 import LandingSocials from '../components/LandingSocials'
 import Cards from '../pages/landing.js';
 
-function DocumentationHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="docs_header">Welcome to anetaBTC Documentation!</h1>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -24,7 +14,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
-      <DocumentationHeader />
         <Cards/>
         <h2 style={{display: 'flex', justifyContent: 'center'}}>Join our community!</h2>
         <LandingSocials/>
